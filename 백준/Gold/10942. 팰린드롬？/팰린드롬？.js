12c770c6f022eafcf9b607092880917846685f25
Yongjae Choi <yongjae.choi20@gmail.com>
2024-02-19 16:58:28 +0900

    [Gold IV] Title: 팰린드롬?, Time: 1836 ms, Memory: 299856 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.js
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.js
@@ -9,20 +9,11 @@ const m = Number(input[2]);
 const questions = input.slice(3).map((v, i) => v.split(" ").map(Number));
 const dp = Array.from(Array(n + 1), () => Array(n + 1).fill(0));
 
-// dp[i-1][j+1] = dp[i][j] && a[i-1] === a[j+1]
-
-// dp[i][i] = 1;
-// dp[i][i+1] = 0;
-
 for (let i = 0; i < n; i += 1) {
   dp[i][i] = 1;
   if (i < n - 1 && a[i] === a[i + 1]) dp[i][i + 1] = 1;
 }
-// for (let i = 0; i < n; i += 1) {
-//   for (let j = i + 2; j < n; j += 1) {
-//     dp[i][j] = Number(dp[i + 1][j - 1] && a[i] === a[j]);
-//   }
-// }
+
 for (let size = 3; size <= n; size += 1) {
   for (let s = 0; s + size - 1 < n; s += 1) {
     const e = s + size - 1;
@@ -30,8 +21,6 @@ for (let size = 3; size <= n; size += 1) {
   }
 }
 
-// [0][1]        [0][1][2]
-// [0][1][2][3]  [0][1][2][3][4]
 const answer = [];
 for (let i = 0; i < m; i += 1) {
   const [s, e] = questions[i];
