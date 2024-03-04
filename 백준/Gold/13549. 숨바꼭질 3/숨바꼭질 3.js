24531f0ba5050c54a25f7e4d05ac141256aa88b6
Yongjae Choi <yongjae.choi20@gmail.com>
2024-03-05 00:03:33 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 928 ms, Memory: 16316 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.js
@@ -0,0 +1,34 @@
+const dest = process.execArgv.includes("--stack-size=65536")
+  ? "/dev/stdin"
+  : "input.txt";
+
+const input = require("fs").readFileSync(dest, "utf-8").split("\n");
+
+const [start, end] = input[0].split(" ").map(Number);
+const bfs = () => {
+  const q = [[start, 0]];
+  const visited = new Array(100001).fill(false);
+  visited[start] = true;
+
+  while (q.length) {
+    const [position, time] = q.shift();
+
+    if (position === end) {
+      console.log(time);
+      return;
+    }
+
+    for (let next of [position * 2, position - 1, position + 1]) {
+      if (next < 0 || next > 100000 || visited[next]) continue;
+
+      if (next === position * 2) {
+        q.unshift([next, time]);
+      } else {
+        q.push([next, time + 1]);
+      }
+      visited[next] = true;
+    }
+  }
+};
+
+bfs();
