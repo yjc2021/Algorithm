bbfdd09ea762562419b69fda7851172b9f04296b
Yongjae Choi <yongjae.choi20@gmail.com>
2024-10-17 17:28:01 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 688 ms, Memory: 84324 KB -BaekjoonHub

---
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.js
@@ -0,0 +1,103 @@
+class PriorityQueue {
+  constructor(comparator) {
+    this.heap = [];
+    this.compare = comparator || ((a,b) => b-a);
+  }
+  size() {
+    return this.heap.length;
+  }
+  enqueue(item) {
+    this.heap.push(item);
+    this.heapifyUp();
+  }
+  dequeue() {
+    const priority = this.heap[0];
+    const end = this.heap.pop();
+    if(this.heap.length) {
+      this.heap[0] = end;
+      this.heapifyDown();
+    }
+    return priority;
+  }
+  heapifyUp() {
+    let index = this.heap.length-1;
+    while(index) {
+      const pIdx = Math.floor((index-1)/2);
+      const p = this.heap[pIdx];
+      const cur = this.heap[index];
+
+      if(this.compare(p, cur) >= 0) break;
+
+      this.heap[index] = p;
+      this.heap[pIdx] = cur;
+      index = pIdx;
+    }
+  }
+  heapifyDown() {
+    let index = 0;
+    const cur = this.heap[index];
+    const len = this.heap.length;
+
+    while(true) {
+      let swap = null;
+      let lc, rc;
+      const lcIdx = index*2+1;
+      const rcIdx = index*2+2;
+
+      if(lcIdx < len) {
+        lc = this.heap[lcIdx];
+        if(this.compare(lc, cur) > 0) swap = lcIdx;
+      }
+      if(rcIdx < len) {
+        rc = this.heap[rcIdx];
+        if(
+          (swap !== null && this.compare(rc, lc) > 0) ||
+          (swap === null && this.compare(rc, cur) > 0)
+        ) swap = rcIdx;
+      }
+      
+      if(swap === null) break;
+      this.heap[index] = this.heap[swap];
+      this.heap[swap] = cur;
+      index = swap;
+    }
+  }
+}
+
+const path = process.platform === 'linux' ? [0, 'utf-8'] : ['input.txt'];
+const input = require('fs').readFileSync(...path).toString().trim().split('\n');
+
+const [n,e] = input[0].split(' ').map(Number);
+const adj = Array.from({length: n+1}, () => []);
+input.slice(1,1+e).forEach(line => {
+  const [a,b,c] = line.split(' ').map(Number);
+  adj[a].push([b,c]);
+  adj[b].push([a,c]);
+})
+const [a,b] = input[1+e].split(' ').map(Number);
+
+function solution(s, e) {
+  const d = Array(n+1).fill(Infinity);
+  const pq = new PriorityQueue((a,b) => b[0] - a[0]);
+
+  d[s] = 0;
+  pq.enqueue([0,s]);
+  while(pq.size()) {
+    const [cost, cur] = pq.dequeue();
+    if(d[cur] !== cost) continue;
+
+    for(const [next, nCost] of adj[cur]) {
+      if(d[next] <= d[cur] + nCost) continue;
+      d[next] = d[cur] + nCost;
+      pq.enqueue([d[next], next]);
+    }
+  }
+
+  return d[e];
+}
+
+const ans1 = solution(1,a) + solution(a,b) + solution(b,n);
+const ans2 = solution(1,b) + solution(b, a) + solution(a,n);
+
+const ans = Math.min(ans1, ans2);
+console.log(ans === Infinity ? -1 : ans);
