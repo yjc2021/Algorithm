60f31b4090f26dc6eeff880ae938faf57a3600b4
Yongjae Choi <yongjae.choi20@gmail.com>
2025-04-07 10:35:53 +0900

    [Silver I] Title: 그림, Time: 300 ms, Memory: 42740 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.js
+++ 백준/Silver/1926. 그림/그림.js
@@ -1,56 +1,74 @@
-const fs = require("node:fs");
-const readline = require("readline");
-const dest = process.execArgv.includes("--stack-size=65536")
-  ? process.stdin
-  : fs.createReadStream("input.txt", "utf-8");
-const rl = readline.createInterface({
-  input: dest,
-  output: process.stdout,
-});
-const input = [];
+const path = process.platform === 'linux' ? [0, 'utf-8'] : ['input.txt'];
+const input = require('fs').readFileSync(...path).toString().trim().split('\n');
 
-rl.on("line", (line) => {
-  input.push(line);
-}).on("close", () => {
-  const [n, m] = input.shift().split(" ").map(Number);
-  const matrix = Array.from(Array(n), () => []);
-  input.forEach((line, index) =>
-    line
-      .split(" ")
-      .map(Number)
-      .forEach((e) => matrix[index].push(e))
-  );
-  console.log(solution(n, m, matrix));
-});
+const [n,m] = input[0].split(' ').map(Number);
+const arr = input.slice(1,n+1).map(line => line.split(' ').map(Number));
 
-const solution = (n, m, matrix) => {
-  const visited = Array.from(Array(n), () => Array(m).fill(0));
-  const dy = [1, -1, 0, 0],
-    dx = [0, 0, 1, -1];
-  let max = 0,
-    cnt = 0;
+const visited = Array.from({length: n}, () => Array(m).fill(0));
 
-  const dfs = (y, x) => {
-    visited[y][x] = 1;
-    let area = 1;
-    for (let i = 0; i < 4; i += 1) {
-      const ny = y + dy[i],
-        nx = x + dx[i];
-      if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-      if (visited[ny][nx] || !matrix[ny][nx]) continue;
+class Queue {
+  constructor() {
+    this.q = [];
+    this.front = 0;
+    this.rear = 0;
+  }
+  enqueue(v) {
+    this.q[this.rear++] = v;
+  }
+  dequeue() {
+    const del = this.q[this.front];
+    delete this.q[this.front++];
+    return del;
+  }
+  size() {
+    return this.rear - this.front;
+  }
+}
+const DIR = [[0,1], [0,-1], [1,0],[-1,0]]
 
-      area += dfs(ny, nx);
-    }
-    return area;
-  };
+function outOfBounds(y,x) {
+  return y < 0 || y >= n || x < 0 || x >= m;
+}
 
-  for (let i = 0; i < n; i += 1) {
-    for (let j = 0; j < m; j += 1) {
-      if (!visited[i][j] && matrix[i][j]) {
-        cnt += 1;
-        max = Math.max(dfs(i, j), max);
-      }
+function dfs(y,x) {
+  const stack = [];
+  stack.push([y,x]);
+  visited[y][x] = 1;
+
+  let area = 1;
+
+  while(stack.length) {
+    const [cy,cx] = stack.pop();
+
+    for([yy,xx] of DIR) {
+      const [ny,nx] = [cy+yy, cx+xx];
+      
+      if(outOfBounds(ny,nx)) continue;
+      if(visited[ny][nx]) continue;
+      if(arr[ny][nx] === 0) continue;
+
+      visited[ny][nx] = 1;
+      stack.push([ny,nx]);
+
+      if(arr[ny][nx] === 1) area += 1;
     }
   }
-  return [cnt, max].join("\n");
-};
+
+  return area;
+}
+
+let maxArea= 0;
+let count = 0;
+
+for(let i = 0; i < n; i+=1) {
+  for(let j = 0; j < m; j+=1) {
+    if(visited[i][j]) continue;
+    if(arr[i][j] === 0) continue;
+    
+    count+=1;
+    maxArea = Math.max(maxArea, dfs(i,j));
+  }
+}
+
+console.log(count);
+console.log(maxArea)
