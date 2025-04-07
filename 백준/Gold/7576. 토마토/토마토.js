f9e274449542e63d58fc14b34dd398c7da74f2e0
Yongjae Choi <yongjae.choi20@gmail.com>
2025-04-07 13:35:00 +0900

    [Gold V] Title: 토마토, Time: 1096 ms, Memory: 165696 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.js
+++ 백준/Gold/7576. 토마토/토마토.js
@@ -1,91 +1,81 @@
-const fs = require("node:fs");
-const readline = require("readline");
-const dest = process.execArgv.includes("--stack-size=65536")
-  ? process.stdin
-  : fs.createReadStream("input.txt", "utf-8");
-const rl = readline.createInterface({
-  input: dest,
-  output: process.stdout,
-});
-const input = [];
-
-rl.on("line", (line) => {
-  input.push(line);
-}).on("close", () => {
-  const [m, n] = input.shift().split(" ").map(Number);
-  const tomatoes = Array.from(Array(n), () => []);
-  input.forEach((line, index) =>
-    line
-      .split(" ")
-      .map(Number)
-      .forEach((e) => tomatoes[index].push(e))
-  );
-  console.log(solution(m, n, tomatoes));
-});
+const path = process.platform === 'linux' ? [0, 'utf-8'] : ['input.txt'];
+const input = require('fs').readFileSync(...path).toString().trim().split('\n');
 
 class Queue {
   constructor() {
     this.q = [];
-    this.left = 0;
-    this.right = 0;
+    this.front = 0;
+    this.rear = 0;
   }
-  enqueue(value) {
-    this.q[this.right++] = value;
+  enqueue(v) {
+    this.q[this.rear++] = v;
   }
   dequeue() {
-    const del = this.q[this.left];
-    delete this.q[this.left++];
+    const del = this.q[this.front];
+    delete this.q[this.front++];
     return del;
   }
   size() {
-    return this.right - this.left;
+    return this.rear - this.front;
   }
 }
 
-const solution = (m, n, tomatoes) => {
-  const dx = [-1, 1, 0, 0],
-    dy = [0, 0, -1, 1];
-  const visited = Array.from(Array(n), () => Array(m).fill(0));
-  const q = new Queue();
+const [m,n] = input[0].split(' ').map(Number);
+const tomatoes = input.slice(1, n+1).map(line=>line.split(' ').map(Number));
+const visited = Array.from({length: n}, () => Array(m).fill(0));
+const dir = [[1,0], [-1,0], [0,1], [0,-1]];
+const q = new Queue();
 
-  for (let i = 0; i < n; i += 1) {
-    for (let j = 0; j < m; j += 1) {
-      if (tomatoes[i][j] === 1) {
-        q.enqueue([i, j]);
-        visited[i][j] = 1;
-      }
+for(let i = 0; i < n; i+=1) {
+  for(let j = 0; j < m; j+=1) {
+    if(tomatoes[i][j] === 1) {
+      q.enqueue([i,j]);
+      visited[i][j] = 1;
     }
   }
-  if (q.size() === m * n) {
-    return 0;
+}
+
+if(q.size() === m*n) {
+  console.log(0);
+  return;
+}
+
+bfs();
+
+let ans = 0;
+for(let i = 0; i < n; i+=1) {
+  for(let j = 0; j < m; j+=1) {
+    if(tomatoes[i][j] === 0) {
+      console.log(-1);
+      return;
+    }
+    ans = Math.max(visited[i][j], ans);
   }
+}
+console.log(ans-1);
 
-  while (q.size()) {
-    const [y, x] = q.dequeue();
+function bfs() {
+  while(q.size()) {
+    const [cy,cx] = q.dequeue();
+    for(let [yy,xx] of dir) {
+      const [ny, nx] = [cy+yy, cx+xx];
+      if(outOfBounds(ny,nx)) continue;
+      if(visited[ny][nx] || tomatoes[ny][nx] === 1 || tomatoes[ny][nx] === -1) continue;
 
-    for (let i = 0; i < 4; i += 1) {
-      const ny = y + dy[i];
-      const nx = x + dx[i];
-      if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-      if (visited[ny][nx]) continue;
-      if (tomatoes[ny][nx] === -1 || tomatoes[ny][nx] === 1) continue;
-
-      q.enqueue([ny, nx]);
+      q.enqueue([ny,nx]);
       tomatoes[ny][nx] = 1;
-      visited[ny][nx] = visited[y][x] + 1;
+      visited[ny][nx] = visited[cy][cx] + 1;
     }
   }
+}
 
-  let res = 0;
-
-  for (let i = 0; i < n; i += 1) {
-    let subMax = 0;
-    for (let j = 0; j < m; j += 1) {
-      if (tomatoes[i][j] === 0) return -1;
-      subMax = Math.max(visited[i][j], subMax);
-    }
-    res = Math.max(subMax, res);
+for(let i = 0; i < n; i+=1) {
+  for(let j = 0; j < m; j+=1) {
+    if(!tomatoes[i][j] || visited[i][j]) continue;
+    bfs(i,j);
   }
+}
 
-  return res - 1;
-};
+function outOfBounds(y,x) {
+  return y < 0 || y >= n || x < 0 || x >= m;
+}
