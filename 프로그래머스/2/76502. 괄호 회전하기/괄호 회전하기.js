6fc6ffd77777a6b00d436fabd7a6a381b8100059
Yongjae Choi <yongjae.choi20@gmail.com>
2025-08-06 16:23:34 +0900

    [level 2] Title: 괄호 회전하기, Time: 12.17 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,29 +1,33 @@
 function solution(s) {
-    const arr = s.split('');
-    const len = arr.length;
-    let ans = 0;
-    for(let i = 0; i<len-1; i+=1) {
-        if(isCorrect(arr)) ans+=1;
-        shiftLeft(arr);
+    let ret = 0;
+    const n = s.length;
+    for(let i = 0; i < n; i+=1) {
+        const stack = [];
+        let isCorrect = true;
+        
+        for(let j = 0; j < n; j+=1) {
+            const c = s[(i+j)%n];
+            
+            if(c === '[' || c === '(' || c === '{') {
+                stack.push(c);
+            } else {
+                if(stack.length === 0) {
+                    isCorrect = false;
+                    break;
+                }
+                
+                const top = stack[stack.length-1];
+                if(c === ']' && top === '[') stack.pop();
+                else if (c===')' && top === '(') stack.pop();
+                else if (c==='}' && top === '{') stack.pop();
+                else {
+                    isCorrect = false;
+                    break;
+                }
+            }
+            
+        }
+        if(isCorrect && stack.length === 0) ret+=1;
     }
-    return ans;
-}
-function isCorrect(s) {
-    st = [];
-    for(c of s) {
-        if(st.length === 0) {st.push(c); continue;}
-        const top = st.pop();
-        if((top === '(' && c === ')') || (top === '{' && c === '}') || (top === '[' && c===']')) continue;
-        st.push(top);
-        st.push(c);
-    }
-    return st.length ? false : true;
-}
-
-function shiftLeft(s) {
-    const tmp = s[0];
-    for(let i = 0; i < s.length-1;i+=1) {
-        s[i] = s[i+1];
-    }
-    s[s.length-1] = tmp;
+    return ret;
 }
