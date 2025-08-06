9f364969140496da53e318b859944bf3c949a91f
Yongjae Choi <yongjae.choi20@gmail.com>
2025-08-06 10:49:50 +0900

    [level 1] Title: 실패율, Time: 12.08 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,11 +1,18 @@
 function solution(N, stages) {
-    const fail = [];
-    let num = stages.length;
-    for(let i = 1; i <= N; i+=1) {
-        const failedNum = stages.filter(v=>v===i).length;
-        fail.push([i, failedNum/num]);
-        num -= failedNum;
+    const challengers = new Array(N+2).fill(0);
+    for(const stage of stages) {
+        challengers[stage] += 1;
     }
-    fail.sort((a,b) => b[1] - a[1]);
-    return fail.map(v => v[0])
+
+    const fail = {};
+    let total = stages.length;
+    
+    for(let i = 1; i <= N; i += 1) {
+        fail[i] = challengers[i] / total;
+        total -= challengers[i];
+    } 
+    
+    const result = Object.entries(fail).sort((a,b) => b[1] - a[1]);
+    
+    return result.map((entry) => Number(entry[0]));
 }
