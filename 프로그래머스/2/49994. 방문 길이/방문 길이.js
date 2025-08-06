4a46129e566312c61b0cc8af263e2cc6980cb138
Yongjae Choi <yongjae.choi20@gmail.com>
2025-08-06 11:46:45 +0900

    [level 2] Title: 방문 길이, Time: 0.77 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -1,24 +1,34 @@
-const dd = {
-    U: [0,1],
-    D: [0,-1],
-    R: [1,0],
-    L: [-1,0]
-}
-
 function solution(dirs) {
-    let pos = [0,0];
+    let x = 0;
+    let y = 0;
     const visited = new Set();
-    for(dir of dirs) {
-        const [nx,ny] = [pos[0] + dd[dir][0], pos[1] + dd[dir][1]];
-        if(isInvalidPos(nx,ny)) continue;
-        visited.add(`${nx}${ny}${pos[0]}${pos[1]}`);
-        visited.add(`${pos[0]}${pos[1]}${nx}${ny}`);
-        pos = [nx,ny]
+    
+    for(const dir of dirs) {
+        const [nx, ny] = updatePosition(x,y, dir);
+        
+        if(!isValidPosition(nx,ny)) continue;
+                
+        visited.add(`${x}${y}${nx}${ny}`);
+        visited.add(`${nx}${ny}${x}${y}`);
+        
+        [x,y] = [nx,ny];
     }
     
     return visited.size/2;
 }
-
-function isInvalidPos(x,y) {
-    return x < -5 || y < -5 || x > 5 || y > 5
+function updatePosition(x,y, dir) {
+    switch(dir) {
+        case "U": 
+            return [x, y+1];
+        case "D": 
+            return [x, y-1];
+        case "L":
+            return [x-1, y];
+        case "R":
+            return [x+1, y];
+    }
 }
+function isValidPosition(x,y) {    
+    return x >= -5 && x <= 5 && y >= -5 && y <= 5
+}
+
